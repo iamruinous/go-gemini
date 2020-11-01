b57ea57fec4b1c29198e0241b52782b293e726a6
adnano <me@adnano.co>
2020-11-01 14:27:49 -0500

    Don't expose DefaultClient

--- gemini.go
+++ gemini.go
@@ -22,31 +22,25 @@ var (
 	ErrInputRequired         = errors.New("gemini: input required")
 )
 
-// DefaultClient is the default client. It is used by Get and Do.
-//
-// On the first request, DefaultClient loads the default list of known hosts.
-var DefaultClient Client
+// defaultClient is the default client. It is used by Get and Do.
+var defaultClient Client
 
 // Get performs a Gemini request for the given url.
-//
-// Get is a wrapper around DefaultClient.Get.
 func Get(url string) (*Response, error) {
 	setupDefaultClientOnce()
-	return DefaultClient.Get(url)
+	return defaultClient.Get(url)
 }
 
 // Do performs a Gemini request and returns a Gemini response.
-//
-// Do is a wrapper around DefaultClient.Do.
 func Do(req *Request) (*Response, error) {
 	setupDefaultClientOnce()
-	return DefaultClient.Do(req)
+	return defaultClient.Do(req)
 }
 
 var defaultClientOnce sync.Once
 
 func setupDefaultClientOnce() {
 	defaultClientOnce.Do(func() {
-		DefaultClient.KnownHosts.LoadDefault()
+		defaultClient.KnownHosts.LoadDefault()
 	})
 }
